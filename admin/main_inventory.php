d295f6e56bc8aaccd385121e60c049a9769ed2af
Your Angelo <angelo18420@gmail.com>
2024-03-07 17:00:48 +0800

    sdsd

--- admin/main_inventory.php
+++ admin/main_inventory.php
@@ -11,7 +11,7 @@
     <!-- Content Header (Page header) -->
     <section class="content-header">
       <h1>
-        User List
+        Main Inventory
       </h1>
       <!-- <ol class="breadcrumb">
         <li><a href="#"><i class="fa fa-dashboard"></i> Home</a></li>
