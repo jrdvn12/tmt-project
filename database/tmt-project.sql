04ff7cc446b784309a090e2d6c825d4395628808
Kate <katescotido@gmail.com>
2024-04-05 16:26:42 +0800

    database

--- database/tmt-project.sql
+++ database/tmt-project.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Apr 04, 2024 at 10:11 AM
+-- Generation Time: Apr 05, 2024 at 10:26 AM
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.2.12
 
@@ -156,7 +156,80 @@ INSERT INTO `audit_trail_record` (`id`, `audit_date`, `audit_time`, `user`, `des
 (516, '2024-04-04', '16:03:42', 'Angelo Cruz', 'Added new Type Raw Materials #MILK date 2024-04-04'),
 (517, '2024-04-04', '16:06:21', 'Angelo Cruz', 'Added new Type Raw Materials #MILK date 2024-04-04'),
 (518, '2024-04-04', '16:07:34', 'Angelo Cruz', 'Added new Type Raw Materials #Sugar date 2024-04-04'),
-(519, '2024-04-04', '16:11:21', 'Angelo Cruz', 'Added new Type Raw Materials #Pure Coffee date 2024-04-04');
+(519, '2024-04-04', '16:11:21', 'Angelo Cruz', 'Added new Type Raw Materials #Pure Coffee date 2024-04-04'),
+(520, '2024-04-05', '14:55:29', '1234 1234', 'Added new Type Raw Materials #Sugar date 2024-04-05'),
+(521, '2024-04-05', '14:56:22', '1234 1234', 'Added new Type Raw Materials #Non-Dairy Creamer date 2024-04-05'),
+(522, '2024-04-05', '14:57:25', '1234 1234', 'Added new Type Raw Materials #Pure Coffee Powder date 2024-04-05'),
+(523, '2024-04-05', '14:58:31', '1234 1234', 'Added new Type Raw Materials #Ground Cofee date 2024-04-05'),
+(524, '2024-04-05', '15:00:15', '1234 1234', 'Added new Type Raw Materials #Coffee Blend date 2024-04-05'),
+(525, '2024-04-05', '15:00:58', '1234 1234', 'Added new Type Raw Materials #Salt date 2024-04-05'),
+(526, '2024-04-05', '15:02:52', '1234 1234', 'Added new Type Raw Materials #HG Pure Coffee date 2024-04-05'),
+(527, '2024-04-05', '15:03:38', '1234 1234', 'Added new Type Raw Materials #PB Coffee Blend date 2024-04-05'),
+(528, '2024-04-05', '15:03:54', '1234 1234', 'Added new Type Raw Materials #MF Ground Coffee date 2024-04-05'),
+(529, '2024-04-05', '15:04:38', '1234 1234', 'Added new Type Raw Materials #Plastic Package  date 2024-04-05'),
+(530, '2024-04-05', '15:04:46', '1234 1234', 'Added new Type Raw Materials #Box date 2024-04-05'),
+(531, '2024-04-05', '15:05:54', '1234 1234', 'Added new Type Raw Materials #ASK Milk date 2024-04-05'),
+(532, '2024-04-05', '15:06:19', '1234 1234', 'Added new Type Raw Materials #FL Caffeine date 2024-04-05'),
+(533, '2024-04-05', '15:06:55', '1234 1234', 'Added new Type Raw Materials #FL Roasted Coffee  date 2024-04-05'),
+(534, '2024-04-05', '15:11:11', '1234 1234', 'Added new Raw Materials # date 2024-04-05'),
+(535, '2024-04-05', '15:13:19', '1234 1234', 'Added new Raw Materials # date 2024-04-05'),
+(536, '2024-04-05', '15:13:56', '1234 1234', 'Added new Raw Materials # date 2024-04-05'),
+(537, '2024-04-05', '15:14:19', '1234 1234', 'Added new Raw Materials # date 2024-04-05'),
+(538, '2024-04-05', '15:14:43', '1234 1234', 'Added new Raw Materials # date 2024-04-05'),
+(539, '2024-04-05', '15:15:16', '1234 1234', 'Added new Raw Materials # date 2024-04-05'),
+(540, '2024-04-05', '15:15:35', '1234 1234', 'Added new Raw Materials # date 2024-04-05'),
+(541, '2024-04-05', '15:15:58', '1234 1234', 'Added new Raw Materials # date 2024-04-05'),
+(542, '2024-04-05', '15:16:23', '1234 1234', 'Added new Raw Materials # date 2024-04-05'),
+(543, '2024-04-05', '15:16:59', '1234 1234', 'Added new Raw Materials # date 2024-04-05'),
+(544, '2024-04-05', '15:17:57', '1234 1234', 'Added new Raw Materials # date 2024-04-05'),
+(545, '2024-04-05', '15:18:20', '1234 1234', 'Added new Type Raw Materials #Powdered Milk date 2024-04-05'),
+(546, '2024-04-05', '15:18:45', '1234 1234', 'Added new Raw Materials # date 2024-04-05'),
+(547, '2024-04-05', '15:23:13', '1234 1234', 'Added new Need Materials # Brand Name date 2024-04-05'),
+(548, '2024-04-05', '15:23:39', '1234 1234', 'Product load needs updated Materials # Brand Name date 2024-04-05'),
+(549, '2024-04-05', '15:23:58', '1234 1234', 'Product load needs updated Materials # Brand Name date 2024-04-05'),
+(550, '2024-04-05', '15:24:16', '1234 1234', 'Product load needs updated Materials # Brand Name date 2024-04-05'),
+(551, '2024-04-05', '15:24:36', '1234 1234', 'Product load needs updated Materials # Brand Name date 2024-04-05'),
+(552, '2024-04-05', '15:25:01', '1234 1234', 'Added new Need Materials # Brand Name date 2024-04-05'),
+(553, '2024-04-05', '15:25:06', '1234 1234', 'Added new Need Materials # Brand Name date 2024-04-05'),
+(554, '2024-04-05', '15:25:11', '1234 1234', 'Added new Need Materials # Brand Name date 2024-04-05'),
+(555, '2024-04-05', '15:30:43', '1234 1234', 'Added new Need Materials # SGR-R date 2024-04-05'),
+(556, '2024-04-05', '15:30:47', '1234 1234', 'Added new Need Materials # COF-HG50 date 2024-04-05'),
+(557, '2024-04-05', '15:33:11', '1234 1234', 'Product load needs updated Materials # SGR-R date 2024-04-05'),
+(558, '2024-04-05', '15:33:16', '1234 1234', 'Product load needs updated Materials # SGR-R date 2024-04-05'),
+(559, '2024-04-05', '15:33:24', '1234 1234', 'Product load needs updated Materials # SGR-R date 2024-04-05'),
+(560, '2024-04-05', '15:33:53', '1234 1234', 'Added new Need Materials # SGR-R date 2024-04-05'),
+(561, '2024-04-05', '15:34:00', '1234 1234', 'Added new Need Materials # NDC date 2024-04-05'),
+(562, '2024-04-05', '15:34:09', '1234 1234', 'Added new Need Materials # COF-HG50 date 2024-04-05'),
+(563, '2024-04-05', '15:34:20', '1234 1234', 'Added new Need Materials # COF-PB6 date 2024-04-05'),
+(564, '2024-04-05', '15:34:29', '1234 1234', 'Added new Need Materials # COF-MF878968 date 2024-04-05'),
+(565, '2024-04-05', '15:34:39', '1234 1234', 'Added new Need Materials # COF-FL722092 date 2024-04-05'),
+(566, '2024-04-05', '15:34:49', '1234 1234', 'Added new Need Materials # SLT-FI date 2024-04-05'),
+(567, '2024-04-05', '15:34:58', '1234 1234', 'Added new Need Materials # ASK date 2024-04-05'),
+(568, '2024-04-05', '15:35:12', '1234 1234', 'Added new Need Materials # SGR-R date 2024-04-05'),
+(569, '2024-04-05', '15:35:19', '1234 1234', 'Product load needs updated Materials # NDC date 2024-04-05'),
+(570, '2024-04-05', '15:35:35', '1234 1234', 'Product load needs updated Materials # COF-HG50 date 2024-04-05'),
+(571, '2024-04-05', '15:36:25', '1234 1234', 'Added new Need Materials # NDC date 2024-04-05'),
+(572, '2024-04-05', '15:36:39', '1234 1234', 'Added new Need Materials # COF-HG50 date 2024-04-05'),
+(573, '2024-04-05', '15:36:49', '1234 1234', 'Added new Need Materials # COF-PB6 date 2024-04-05'),
+(574, '2024-04-05', '15:37:07', '1234 1234', 'Added new Need Materials # COF-MF878968 date 2024-04-05'),
+(575, '2024-04-05', '15:37:17', '1234 1234', 'Added new Need Materials # COF-FL578906 date 2024-04-05'),
+(576, '2024-04-05', '15:37:25', '1234 1234', 'Added new Need Materials # COF-FL722092 date 2024-04-05'),
+(577, '2024-04-05', '15:37:38', '1234 1234', 'Added new Need Materials # SLT-FI date 2024-04-05'),
+(578, '2024-04-05', '15:37:51', '1234 1234', 'Added new Need Materials # ASK date 2024-04-05'),
+(579, '2024-04-05', '15:38:05', '1234 1234', 'Added new Need Materials # MLK-704217 date 2024-04-05'),
+(580, '2024-04-05', '15:38:16', '1234 1234', 'Added new Need Materials # SGR-R date 2024-04-05'),
+(581, '2024-04-05', '15:38:27', '1234 1234', 'Added new Need Materials # NDC date 2024-04-05'),
+(582, '2024-04-05', '15:38:42', '1234 1234', 'Added new Need Materials # COF-HG50 date 2024-04-05'),
+(583, '2024-04-05', '15:38:48', '1234 1234', 'Added new Need Materials # COF-PB6 date 2024-04-05'),
+(584, '2024-04-05', '15:39:10', '1234 1234', 'Added new Need Materials # COF-FL578906 date 2024-04-05'),
+(585, '2024-04-05', '15:39:21', '1234 1234', 'Added new Need Materials # COF-FL722092 date 2024-04-05'),
+(586, '2024-04-05', '15:39:38', '1234 1234', 'Added new Need Materials # COF-MF878968 date 2024-04-05'),
+(587, '2024-04-05', '15:39:48', '1234 1234', 'Product load needs updated Materials # SLT-FI date 2024-04-05'),
+(588, '2024-04-05', '15:40:07', '1234 1234', 'Added new Need Materials # SLT-FI date 2024-04-05'),
+(589, '2024-04-05', '15:40:20', '1234 1234', 'Added new Need Materials # ASK date 2024-04-05'),
+(590, '2024-04-05', '15:41:14', '1234 1234', 'Added new Type Raw Materials #Sugar B date 2024-04-05'),
+(591, '2024-04-05', '15:41:37', '1234 1234', 'Added new Raw Materials # date 2024-04-05'),
+(592, '2024-04-05', '15:41:54', '1234 1234', 'Added new Need Materials # SGR-B578903 date 2024-04-05');
 
 -- --------------------------------------------------------
 
@@ -239,6 +312,14 @@ CREATE TABLE `production` (
   `production_expiration` date NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
+--
+-- Dumping data for table `production`
+--
+
+INSERT INTO `production` (`id`, `material_code`, `product_name`, `product_batch`, `production_status`, `production_pieces`, `production_kilo`, `production_date`, `production_expiration`) VALUES
+(20, '5', 'Café Gusto 3-in-1 Premium Taste Coffee Mix (Clasico)', '55565465', 'Preparing', 0, 0, '2024-04-05', '2025-04-05'),
+(21, '7', 'Café Gusto 3-in-1 Premium Taste Coffee Mix (White) ', '100', 'Preparing', 0, 0, '2024-04-05', '2025-04-05');
+
 -- --------------------------------------------------------
 
 --
@@ -258,10 +339,34 @@ CREATE TABLE `product_needs` (
 --
 
 INSERT INTO `product_needs` (`id`, `product_id`, `product_name`, `item_need`, `loads`) VALUES
-(61, '5', 'Café Gusto 3-in-1 Premium Taste Coffee Mix (Clasico)', 'Creamy Corp', 50),
-(62, '6', 'Café Gusto 3-in-1 Premium Taste Coffee Mix (Brown) ', 'Creamy Corp', 20),
-(63, '5', 'Café Gusto 3-in-1 Premium Taste Coffee Mix (Clasico)', 'Made in China', 20),
-(64, '7', 'Café Gusto 3-in-1 Premium Taste Coffee Mix (White) ', 'Creamy Corp', 20);
+(71, '5', 'Café Gusto 3-in-1 Premium Taste Coffee Mix (Clasico)', 'SGR-R', 150.87),
+(72, '5', 'Café Gusto 3-in-1 Premium Taste Coffee Mix (Clasico)', 'NDC', 11646),
+(73, '5', 'Café Gusto 3-in-1 Premium Taste Coffee Mix (Clasico)', 'COF-HG50', 15),
+(74, '5', 'Café Gusto 3-in-1 Premium Taste Coffee Mix (Clasico)', 'COF-PB6', 15),
+(75, '5', 'Café Gusto 3-in-1 Premium Taste Coffee Mix (Clasico)', 'COF-MF878968', 0.675),
+(76, '5', 'Café Gusto 3-in-1 Premium Taste Coffee Mix (Clasico)', 'COF-FL722092', 0.675),
+(77, '5', 'Café Gusto 3-in-1 Premium Taste Coffee Mix (Clasico)', 'SLT-FI', 1.08),
+(78, '5', 'Café Gusto 3-in-1 Premium Taste Coffee Mix (Clasico)', 'ASK', 0.105),
+(79, '7', 'Café Gusto 3-in-1 Premium Taste Coffee Mix (White) ', 'SGR-R', 141.45),
+(80, '7', 'Café Gusto 3-in-1 Premium Taste Coffee Mix (White) ', 'NDC', 133.92),
+(81, '7', 'Café Gusto 3-in-1 Premium Taste Coffee Mix (White) ', 'COF-HG50', 10.71),
+(82, '7', 'Café Gusto 3-in-1 Premium Taste Coffee Mix (White) ', 'COF-PB6', 10.71),
+(83, '7', 'Café Gusto 3-in-1 Premium Taste Coffee Mix (White) ', 'COF-MF878968', 1.065),
+(84, '7', 'Café Gusto 3-in-1 Premium Taste Coffee Mix (White) ', 'COF-FL578906', 0.645),
+(85, '7', 'Café Gusto 3-in-1 Premium Taste Coffee Mix (White) ', 'COF-FL722092', 0.525),
+(86, '7', 'Café Gusto 3-in-1 Premium Taste Coffee Mix (White) ', 'SLT-FI', 0.435),
+(87, '7', 'Café Gusto 3-in-1 Premium Taste Coffee Mix (White) ', 'ASK', 0.435),
+(88, '5', 'Café Gusto 3-in-1 Premium Taste Coffee Mix (Clasico)', 'MLK-704217', 0.435),
+(89, '6', 'Café Gusto 3-in-1 Premium Taste Coffee Mix (Brown) ', 'SGR-R', 146.64),
+(90, '6', 'Café Gusto 3-in-1 Premium Taste Coffee Mix (Brown) ', 'NDC', 121.98),
+(91, '6', 'Café Gusto 3-in-1 Premium Taste Coffee Mix (Brown) ', 'COF-HG50', 14.43),
+(92, '6', 'Café Gusto 3-in-1 Premium Taste Coffee Mix (Brown) ', 'COF-PB6', 14.43),
+(93, '6', 'Café Gusto 3-in-1 Premium Taste Coffee Mix (Brown) ', 'COF-FL578906', 0.66),
+(94, '6', 'Café Gusto 3-in-1 Premium Taste Coffee Mix (Brown) ', 'COF-FL722092', 0.54),
+(95, '6', 'Café Gusto 3-in-1 Premium Taste Coffee Mix (Brown) ', 'COF-MF878968', 0.225),
+(96, '6', 'Café Gusto 3-in-1 Premium Taste Coffee Mix (Brown) ', 'SLT-FI', 0.54),
+(97, '6', 'Café Gusto 3-in-1 Premium Taste Coffee Mix (Brown) ', 'ASK', 0.105),
+(98, '6', 'Café Gusto 3-in-1 Premium Taste Coffee Mix (Brown) ', 'SGR-B578903', 0.45);
 
 -- --------------------------------------------------------
 
@@ -320,7 +425,41 @@ INSERT INTO `product_needs_history` (`id`, `product_id`, `product_name`, `item_n
 (36, '5', 'Café Gusto 3-in-1 Premium Taste Coffee Mix (Clasico)', 'Creamy Corp', 5),
 (37, '6', 'Café Gusto 3-in-1 Premium Taste Coffee Mix (Brown) ', 'Creamy Corp', 20),
 (38, '5', 'Café Gusto 3-in-1 Premium Taste Coffee Mix (Clasico)', 'Made in China', 20),
-(39, '7', 'Café Gusto 3-in-1 Premium Taste Coffee Mix (White) ', 'Creamy Corp', 20);
+(39, '7', 'Café Gusto 3-in-1 Premium Taste Coffee Mix (White) ', 'Creamy Corp', 20),
+(40, '5', 'Café Gusto 3-in-1 Premium Taste Coffee Mix (Clasico)', 'Brand Name', 150.87),
+(41, '7', 'Café Gusto 3-in-1 Premium Taste Coffee Mix (White) ', 'Brand Name', 10),
+(42, '6', 'Café Gusto 3-in-1 Premium Taste Coffee Mix (Brown) ', 'Brand Name', 5),
+(43, '5', 'Café Gusto 3-in-1 Premium Taste Coffee Mix (Clasico)', 'Brand Name', 5),
+(44, '5', 'Café Gusto 3-in-1 Premium Taste Coffee Mix (Clasico)', 'SGR-R', 116.46),
+(45, '5', 'Café Gusto 3-in-1 Premium Taste Coffee Mix (Clasico)', 'COF-HG50', 5),
+(46, '5', 'Café Gusto 3-in-1 Premium Taste Coffee Mix (Clasico)', 'SGR-R', 150.87),
+(47, '5', 'Café Gusto 3-in-1 Premium Taste Coffee Mix (Clasico)', 'NDC', 116.46),
+(48, '5', 'Café Gusto 3-in-1 Premium Taste Coffee Mix (Clasico)', 'COF-HG50', 15),
+(49, '5', 'Café Gusto 3-in-1 Premium Taste Coffee Mix (Clasico)', 'COF-PB6', 15),
+(50, '5', 'Café Gusto 3-in-1 Premium Taste Coffee Mix (Clasico)', 'COF-MF878968', 0.675),
+(51, '5', 'Café Gusto 3-in-1 Premium Taste Coffee Mix (Clasico)', 'COF-FL722092', 0.675),
+(52, '5', 'Café Gusto 3-in-1 Premium Taste Coffee Mix (Clasico)', 'SLT-FI', 0.54),
+(53, '5', 'Café Gusto 3-in-1 Premium Taste Coffee Mix (Clasico)', 'ASK', 0.105),
+(54, '7', 'Café Gusto 3-in-1 Premium Taste Coffee Mix (White) ', 'SGR-R', 141.45),
+(55, '7', 'Café Gusto 3-in-1 Premium Taste Coffee Mix (White) ', 'NDC', 133.92),
+(56, '7', 'Café Gusto 3-in-1 Premium Taste Coffee Mix (White) ', 'COF-HG50', 10.71),
+(57, '7', 'Café Gusto 3-in-1 Premium Taste Coffee Mix (White) ', 'COF-PB6', 10.71),
+(58, '7', 'Café Gusto 3-in-1 Premium Taste Coffee Mix (White) ', 'COF-MF878968', 1.065),
+(59, '7', 'Café Gusto 3-in-1 Premium Taste Coffee Mix (White) ', 'COF-FL578906', 0.645),
+(60, '7', 'Café Gusto 3-in-1 Premium Taste Coffee Mix (White) ', 'COF-FL722092', 0.525),
+(61, '7', 'Café Gusto 3-in-1 Premium Taste Coffee Mix (White) ', 'SLT-FI', 0.435),
+(62, '7', 'Café Gusto 3-in-1 Premium Taste Coffee Mix (White) ', 'ASK', 0.435),
+(63, '5', 'Café Gusto 3-in-1 Premium Taste Coffee Mix (Clasico)', 'MLK-704217', 0.435),
+(64, '6', 'Café Gusto 3-in-1 Premium Taste Coffee Mix (Brown) ', 'SGR-R', 146.64),
+(65, '6', 'Café Gusto 3-in-1 Premium Taste Coffee Mix (Brown) ', 'NDC', 121.98),
+(66, '6', 'Café Gusto 3-in-1 Premium Taste Coffee Mix (Brown) ', 'COF-HG50', 14.43),
+(67, '6', 'Café Gusto 3-in-1 Premium Taste Coffee Mix (Brown) ', 'COF-PB6', 14.43),
+(68, '6', 'Café Gusto 3-in-1 Premium Taste Coffee Mix (Brown) ', 'COF-FL578906', 0.66),
+(69, '6', 'Café Gusto 3-in-1 Premium Taste Coffee Mix (Brown) ', 'COF-FL722092', 0.54),
+(70, '6', 'Café Gusto 3-in-1 Premium Taste Coffee Mix (Brown) ', 'COF-MF878968', 0.225),
+(71, '6', 'Café Gusto 3-in-1 Premium Taste Coffee Mix (Brown) ', 'SLT-FI', 0.54),
+(72, '6', 'Café Gusto 3-in-1 Premium Taste Coffee Mix (Brown) ', 'ASK', 0.105),
+(73, '6', 'Café Gusto 3-in-1 Premium Taste Coffee Mix (Brown) ', 'SGR-B578903', 0.45);
 
 -- --------------------------------------------------------
 
@@ -346,10 +485,19 @@ CREATE TABLE `raw_materials` (
 --
 
 INSERT INTO `raw_materials` (`id`, `material_code`, `material_type`, `material_name`, `material_batch`, `loads`, `material_usage`, `material_remaining`, `dateofstock`, `date_expiration`) VALUES
-(5, 'TM44511', 'Creamer', 'Creamy Corp', '20', 300, 0, 300, '2024-03-15', '2024-10-31'),
-(6, 'TM45682', 'Salt', 'Salt Company', '5', 200, 0, 200, '2024-03-15', '2025-01-10'),
-(7, 'TM45123', 'Plastic', 'Made in China', '5', 400, 0, 400, '2024-03-15', '2025-09-06'),
-(8, 'TM156394', 'Pure Coffee', 'Thailand', '10', 900, 0, 900, '2024-03-15', '2024-09-30');
+(22, 'SGR-R', '7', 'Brand Name', '50', 10000, 0, 0, '2024-04-05', '2024-07-10'),
+(23, 'NDC', '8', 'Brand Name', '10235478', 10000, 0, 0, '2024-04-05', '2024-07-10'),
+(24, 'COF-HG50', '13', 'Brand Name', '145874', 10000, 0, 0, '2024-04-05', '2024-09-01'),
+(25, 'COF-PB6', '14', 'Brand Name', '4573651', 10000, 0, 0, '2024-04-05', '2024-10-01'),
+(26, 'COF-MF878968', '15', 'Brand Name', '45003697', 10000, 0, 0, '2024-04-05', '2024-06-25'),
+(27, 'COF-FL578906', '19', 'Brand Name', '00236974', 10000, 0, 0, '2024-04-05', '2025-01-01'),
+(28, 'COF-FL722092', '20', 'Brand Name', '00369745', 10000, 0, 0, '2024-04-05', '2025-01-01'),
+(29, 'SLT-FI', '12', 'Brand Name', '11236954', 10000, 0, 0, '2024-04-05', '2026-01-01'),
+(30, 'ASK', '18', 'Brand Name', '4456312', 10000, 0, 0, '2024-04-05', '2024-05-30'),
+(31, 'BOX-00', '17', 'Brand Name', '66542137', 5000, 0, 0, '2024-04-05', '2026-02-01'),
+(32, 'PPK-0145', '16', 'Brand Name', '88451230', 5000, 0, 0, '2024-04-05', '2026-02-01'),
+(33, 'MLK-704217', '21', 'Brand Name', '33125947', 10000, 0, 0, '2024-04-05', '2024-08-01'),
+(34, 'SGR-B578903', '22', 'Brand Name', '4458712', 10000, 0, 0, '2024-04-05', '2024-11-02');
 
 -- --------------------------------------------------------
 
@@ -367,7 +515,19 @@ CREATE TABLE `type_raw_materials` (
 --
 
 INSERT INTO `type_raw_materials` (`id`, `material_type`) VALUES
-(6, 'Pure Coffee');
+(7, 'Sugar'),
+(8, 'Non-Dairy Creamer'),
+(12, 'Salt'),
+(13, 'HG Pure Coffee'),
+(14, 'PB Coffee Blend'),
+(15, 'MF Ground Coffee'),
+(16, 'Plastic Package '),
+(17, 'Box'),
+(18, 'ASK Milk'),
+(19, 'FL Caffeine'),
+(20, 'FL Roasted Coffee '),
+(21, 'Powdered Milk'),
+(22, 'Sugar B');
 
 --
 -- Indexes for dumped tables
@@ -447,7 +607,7 @@ ALTER TABLE `admin`
 -- AUTO_INCREMENT for table `audit_trail_record`
 --
 ALTER TABLE `audit_trail_record`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=520;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=593;
 
 --
 -- AUTO_INCREMENT for table `main_inventory`
@@ -471,31 +631,31 @@ ALTER TABLE `product`
 -- AUTO_INCREMENT for table `production`
 --
 ALTER TABLE `production`
-  MODIFY `id` int(255) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=19;
+  MODIFY `id` int(255) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=22;
 
 --
 -- AUTO_INCREMENT for table `product_needs`
 --
 ALTER TABLE `product_needs`
-  MODIFY `id` int(255) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=65;
+  MODIFY `id` int(255) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=99;
 
 --
 -- AUTO_INCREMENT for table `product_needs_history`
 --
 ALTER TABLE `product_needs_history`
-  MODIFY `id` int(255) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=40;
+  MODIFY `id` int(255) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=74;
 
 --
 -- AUTO_INCREMENT for table `raw_materials`
 --
 ALTER TABLE `raw_materials`
-  MODIFY `id` int(255) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=22;
+  MODIFY `id` int(255) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=35;
 
 --
 -- AUTO_INCREMENT for table `type_raw_materials`
 --
 ALTER TABLE `type_raw_materials`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=23;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
