47eb64c33ae3a456d6b1fb577d1c2d9c5a8db9ea
Your Angelo <angelo18420@gmail.com>
2024-04-11 17:17:49 +0800

    user panel

--- admin/home.php
+++ admin/home.php
@@ -128,7 +128,7 @@
             <div class="icon">
               <i class="fa fa-cogs"></i>
             </div>
-            <a href="production" class="small-box-footer">More info <i class="fa fa-arrow-circle-right"></i></a>
+            <a href="lost" class="small-box-footer">More info <i class="fa fa-arrow-circle-right"></i></a>
           </div>
         </div>
         <!-- ./col -->
