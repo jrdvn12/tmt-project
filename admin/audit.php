4339f12b8fbc72a790185538df940aa1d5339b6d
Jared <jaredivan.bruno@tup.edu.ph>
2024-04-11 11:52:33 +0800

    new trip

--- admin/audit.php
+++ admin/audit.php
@@ -88,11 +88,11 @@
           </div>
         </div>
       </div>
-    </section>   
-  </div>
-    
-  <?php include 'includes/footer.php'; ?>
-  <?php include 'includes/audit_modal.php'; ?>
+      </section>   
+</div>
+
+<?php include 'includes/footer.php'; ?>
+<?php include 'includes/audit_modal.php'; ?>
 </div>
 <?php include 'includes/scripts.php'; ?>
 <script>
@@ -128,6 +128,15 @@ function getRow(id){
   });
 }
 
+// Scroll to top function
+function scrollToTop() {
+    document.body.scrollTop = 0; // For Safari
+    document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
 </script>
+
+<!-- Scroll to top button -->
+<button onclick="scrollToTop()" id="scrollToTopBtn" title="Go to top">Top</button>
+
 </body>
 </html>
