02d745883302ce33f0e2c29ee3164b6c82d3c741
Kate <katescotido@gmail.com>
2024-04-11 11:12:00 +0800

    home colors

--- admin/includes/menubar.php
+++ admin/includes/menubar.php
@@ -4,24 +4,25 @@
 <aside class="main-sidebar"  style="height:100%; " >
     <!-- sidebar: style can be found in sidebar.less -->
     <section class="sidebar" >
-      <!-- Sidebar user panel -->
-      <div class="user-panel">
-        <div class="pull-left image">
-          <img src="<?php echo (!empty($user['photo'])) ? '../images/'.$user['photo'] : '../images/profile.jpg'; ?>" class="img-circle" alt="User Image">
-        </div>
-        <div class="pull-left info">
-          <p><?php echo $user['firstname'].' '.$user['lastname']; ?></p>
-          <a><i class="fa fa-circle text-success"></i> Online</a>
-        </div>
-      </div>
+<!-- Sidebar user panel -->
+<div class="user-panel" style="color: black; background-color: white;">
+  <div class="pull-left image">
+    <img src="<?php echo (!empty($user['photo'])) ? '../images/'.$user['photo'] : '../images/profile.jpg'; ?>" class="img-circle" alt="User Image">
+  </div>
+  <div class="pull-left info">
+    <p><?php echo $user['firstname'].' '.$user['lastname']; ?></p>
+    <a style="color: black;"><i class="fa fa-circle text-success"></i> Online</a>
+  </div>
+</div>
+
       <!-- sidebar menu: : style can be found in sidebar.less -->
       <ul class="sidebar-menu" data-widget="tree">
         <li class="header"></li>
-        <li class=""><a href="home"><i class="fa fa-home"></i> <span>Home</span></a></li>
+        <li class=""><a href="home" style="color: black;"><i class="fa fa-home"></i> <span>Home</span></a></li>
         <li class="header">MANAGE</li>   
-        <li><a href="order"><i class="fa fa-shopping-cart"></i> <span>Order</span></a></li>     
-              <li class="treeview">
-                <a href="#">
+        <li><a href="order" style="color: black;"><i class="fa fa-shopping-cart"></i> <span>Order</span></a></li>     
+              <li class="treeview" style="color: black;">
+                <a href="#" style="color: black;">
                   <i class="fa fa-bar-chart"></i>
                   <span>Sales</span>
                   <span class="pull-right-container">
@@ -29,13 +30,13 @@
                   </span>
                 </a>
                 <ul class="treeview-menu">
-                  <li><a href="#"><i class="fa fa-circle-o"></i> Main Sales</a></li>    
-                  <li><a href="#"><i class="fa fa-circle-o"></i> Distributor Sales</a></li>
-                  <li><a href="#"><i class="fa fa-circle-o"></i> Seller Sales</a></li>        
+                  <li><a href="#" style="color: black;"><i class="fa fa-circle-o" ></i> Main Sales</a></li>    
+                  <li><a href="#" style="color: black;"><i class="fa fa-circle-o"></i> Distributor Sales</a></li>
+                  <li><a href="#" style="color: black;"><i class="fa fa-circle-o"></i> Seller Sales</a></li>        
                 </ul>
               </li> 
         <li class="treeview">
-          <a href="#">
+          <a href="#" style="color: black;">
             <i class="fa fa-dropbox"></i>
             <span>Inventory</span>
             <span class="pull-right-container">
@@ -43,14 +44,14 @@
             </span>
           </a>
           <ul class="treeview-menu">
-            <li><a href="main_inventory"><i class="fa fa-circle-o"></i> Main Inventory</a></li>    
+            <li><a href="main_inventory" style="color: black;"><i class="fa fa-circle-o"></i> Main Inventory</a></li>    
             
-            <li><a href="distributor_inventory"><i class="fa fa-circle-o"></i> Distributor Inventory</a></li>
-            <li><a href="#"><i class="fa fa-circle-o"></i> Inventory History</a></li>
+            <li><a href="distributor_inventory" style="color: black;"><i class="fa fa-circle-o"></i> Distributor Inventory</a></li>
+            <li><a href="#" style="color: black;"><i class="fa fa-circle-o"></i> Inventory History</a></li>
           </ul>
         </li>
         <li class="treeview">
-          <a href="#">
+          <a href="#" style="color: black;">
             <i class="fa fa-shopping-basket"></i>
             <span>Raw Materials</span>
             <span class="pull-right-container">
@@ -58,16 +59,16 @@
             </span>
           </a>
           <ul class="treeview-menu">
-            <li><a href="main_raw_materials"><i class="fa fa-circle-o"></i> Main Raw Materials</a></li>    
-            <li><a href="raw_materials"><i class="fa fa-circle-o"></i> Raw Materials Inventory</a></li>
-            <li><a href="type_raw_materials"><i class="fa fa-circle-o"></i>  Raw Materials Type</a></li>
+            <li><a href="main_raw_materials" style="color: black;"><i class="fa fa-circle-o"></i> Main Raw Materials</a></li>    
+            <li><a href="raw_materials" style="color: black;"><i class="fa fa-circle-o"></i> Raw Materials Inventory</a></li>
+            <li><a href="type_raw_materials" style="color: black;"><i class="fa fa-circle-o"></i>  Raw Materials Type</a></li>
             <!-- <li><a href="raw_materials_inventory"><i class="fa fa-circle-o"></i> Raw Materials Inventory</a></li> -->
             
           </ul>
         </li>
 
         <li class="treeview">
-          <a href="#">
+          <a href="#"style="color: black;">
             <i class="fa fa fa-cogs"></i>
             <span>Product Management</span>
             <span class="pull-right-container">
@@ -75,12 +76,12 @@
             </span>
           </a>
           <ul class="treeview-menu">
-          <li><a href="product"><i class=" fa fa-circle-o"></i> <span>Product</span></a></li>
-          <li><a href="product_needs"><i class=" fa fa-circle-o"></i> <span>Product Needs</span></a></li>
+          <li><a href="product" style="color: black;"><i class=" fa fa-circle-o"></i> <span>Product</span></a></li>
+          <li><a href="product_needs" style="color: black;"><i class=" fa fa-circle-o"></i> <span>Product Needs</span></a></li>
 
-          <li><a href="costing"><i class="fa fa-circle-o"></i> <span>Costing Raw Materials</span></a></li>
+          <li><a href="costing" style="color: black;"><i class="fa fa-circle-o"></i> <span>Costing Raw Materials</span></a></li>
           
-          <li><a href="freebies"><i class="fa fa-circle-o"></i> <span>Gift</span></a></li>
+          <li><a href="freebies" style="color: black;"><i class="fa fa-circle-o"></i> <span>Gift</span></a></li>
           
           
             
@@ -88,7 +89,7 @@
         </li>
 
         <li class="treeview">
-          <a href="#">
+          <a href="#" style="color: black;">
             <i class="fa fa-building-o"></i>
             <span>Warehouse Management</span>
             <span class="pull-right-container">
@@ -96,8 +97,8 @@
             </span>
           </a>
           <ul class="treeview-menu">
-          <li><a href="production"><i class=" fa fa-circle-o"></i> <span>Production</span></a></li>
-          <li><a href="lost"><i class="fa fa-circle-o"></i> <span>Loss</span></a></li>
+          <li><a href="production" style="color: black;"><i class=" fa fa-circle-o"></i> <span>Production</span></a></li>
+          <li><a href="lost" style="color: black;"><i class="fa fa-circle-o"></i> <span>Loss</span></a></li>
          
           
             
@@ -105,12 +106,12 @@
         </li>
         
         
-        <li><a href="user"><i class="fa fa-user"></i> <span>User</span></a></li>
-        <li><a href="audit"><i class="fa fa-history"></i> <span>Audit Trail Record</span></a></li>
+        <li><a href="user" style="color: black;"><i class="fa fa-user"></i> <span>User</span></a></li>
+        <li><a href="audit" style="color: black;"><i class="fa fa-history"></i> <span>Audit Trail Record</span></a></li>
         <li class="header">PRINTABLES</li>
-        <li><a href="#"><i class="fa fa-bar-chart"></i> <span>Sales</span></a></li>
-        <li><a href="#"><i class="fa fa-dropbox"></i> <span>Inventory</span></a></li>
-        <li><a href="#"><i class="fa fa-truck"></i><span>Delivery</span></a></li>
+        <li><a href="#" style="color: black;"><i class="fa fa-bar-chart"></i> <span>Sales</span></a></li>
+        <li><a href="#" style="color: black;"><i class="fa fa-dropbox"></i> <span>Inventory</span></a></li>
+        <li><a href="#" style="color: black;"><i class="fa fa-truck"></i><span>Delivery</span></a></li>
         <!-- <li class="header">
         <div class="hidden-xs">
           <b>All rights reserved</b>
--- admin/includes/navbar.php
+++ admin/includes/navbar.php
@@ -3,15 +3,15 @@
     <!-- Logo -->
     <a href="home" class="logo">
       <!-- mini logo for sidebar mini 50x50 pixels -->
-      <span class="logo-mini"><b>TMT</b></span>
+      <span class="logo-mini" style="color: black;"><b>TMT</b></span>
       <!-- logo for regular state and mobile devices -->
-      <span class="logo-lg">TMT Foods Inc</span>
+      <span class="logo-lg" style="color: black;">TMT Foods Inc</span>
     </a>
     <!-- Header Navbar: style can be found in header.less -->
     <nav class="navbar navbar-static-top">
       <!-- Sidebar toggle button-->
-      <a href="#" class="sidebar-toggle" data-toggle="push-menu" role="button">
-        <span class="sr-only">Toggle navigation</span>
+      <a href="#" class="sidebar-toggle" data-toggle="push-menu" role="button" style="color: black;">
+        <span class="sr-only" >Toggle navigation</span>
       </a>
       <div class="navbar-custom-menu">
         <ul class="nav navbar-nav">
