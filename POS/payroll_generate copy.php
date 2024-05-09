73448166ee1669fda717a87b8d30e85e51e02545
Your Angelo <angelo18420@gmail.com>
2024-05-09 14:55:36 +0800

    RESIBO

--- POS/payroll_generate copy.php
+++ POS/payroll_generate copy.php
@@ -1,12 +1,12 @@
 <?php
 	include 'includes/session.php';
 
-	function generateRow($from, $to, $conn){
+	function generateRow($conn){
 		$contents = '';
 	 	
 		//$sql = "SELECT * FROM payslip WHERE datefrom >= '$from' AND dateto <= '$to'";
 
-		$sql = "SELECT * FROM payslip WHERE datefrom >= '$from' AND dateto <= '$to'";
+		$sql = "SELECT * FROM main_inventory ";
 
 
 		$query = $conn->query($sql);
@@ -16,11 +16,11 @@
 			
 			$contents .= '
 			<tr>
-				<td align="center">'.$row['employee_name'].'</td>
-				<td align="center">'.$row['employee_id'].'</td>
-				<td align="center">'.$row['employee_id'].'</td>
-				<td align="center">'.$row['paystatus'].'</td>
-				<td align="center">'.number_format($row['netpay'], 2).'</td>
+			<td align="center">'.$row['product_id'].'</td>
+			<td align="center">'.$row['product_number'].'</td>
+			<td align="center">'.$row['qty'].'</td>
+			
+			<td align="center">'.number_format($row['price'], 2).'</td>
 			</tr>
 			';
 			
@@ -37,18 +37,14 @@
 		return $contents;
 	}
 		
-	$range = $_POST['date_range'];
-	$ex = explode(' - ', $range);
-	$from = date('Y-m-d', strtotime($ex[0]));
-	$to = date('Y-m-d', strtotime($ex[1]));
+
 
 	
 
-	$from_title = date('M d, Y', strtotime($ex[0]));
-	$to_title = date('M d, Y', strtotime($ex[1]));
+	
 
 	require_once('../tcpdf/tcpdf.php');  
-    $pdf = new TCPDF('L', PDF_UNIT, PDF_PAGE_FORMAT, true, 'UTF-8', false);  
+    $pdf = new TCPDF('P', PDF_UNIT, PDF_PAGE_FORMAT, true, 'UTF-8', false);  
     $pdf->SetCreator(PDF_CREATOR);  
     $pdf->SetTitle('EZ PAYROLL SYSTEM');  
     $pdf->SetHeaderData('', '', PDF_HEADER_TITLE, PDF_HEADER_STRING);  
@@ -65,31 +61,20 @@
     $content = '';  
     $content .= '
 	<h2 align="center">EZ PAYROLL SYSTEM</h2>
-	<h4 align="center">'.$from_title." - ".$to_title.'</h4>
+	<h4 align="center"></h4>
 	<table border="1" cellspacing="0" cellpadding="3">  
 	<tr>  
 		<th width="3%"  align="center"><b>No</b></th>
-		<th width="8%"  align="center"><b>Name</b></th>
-		<th width="5%"  align="center"><b>Position</b></th>
-		<th width="6%"  align="center"><b>Employee ID</b></th>
-		<th width="6%"  align="center"><b>Monthly Salary</b></th>
-		<th width="6%"  align="center" colspan="2"><b>Other Compensation</b></th>
-		<th width="6%" align="center" ><b>Gross Amount </b></th>
-		<th width="6%" colspan="6" align="center" ><b>Deductions</b></th>
-		<th width="6%" align="center" ><b>TOTAL DEDUCTIONS</b></th>
-		<th width="6%"  align="center" ><b>NET AMOUNT DUE<br>* 1st half<br>. 2nd half</b></th>
+		<th width="8%"  align="center"><b>Code</b></th>
+		<th width="5%"  align="center"><b>Balance</b></th>
+		<th width="6%"  align="center"><b>Price</b></th>
+
 		
 	</tr>
-	<tr>
-			<td colspan="4"></td> <!-- Empty cells for Employee Name, Position, Employee ID, and Monthly Salary -->
-			<th width="6%" align="center"><b>Personal Economic Relief Allowance</b></th>
-			<th width="6%" align="center"><b>Additional Compens.</b></th>
-			<td></td> <!-- Empty cell for Status -->
-			<td></td> <!-- Empty cell for Net Pay -->
-	</tr>
+	
 
       ';  
-    $content .= generateRow($from, $to, $conn);  
+    $content .= generateRow( $conn);  
     $content .= '</table>';  
     $pdf->writeHTML($content);  
     $pdf->Output('payroll.pdf', 'I');
